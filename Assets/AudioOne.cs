ac8c254b8fe584af1fde33be29a8e1500ba84d6a
Afreda323 <anthonyfreda323@gmail.com>
2018-03-30 22:15:16 -0400

    Simple map texture and layout. Music and ship asset. Splash Screen

---
+++ Assets/AudioOne.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioOne : MonoBehaviour {
+    [SerializeField] float splashTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("GoToGame", splashTime);
+	}
+	
+    void GoToGame () {
+        SceneManager.LoadScene(1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Alien Escape
+On rails shooter placed on another planet. Still Conceptual
+
+### Concept Screenshot
+
+![Screenshot](/screenshots/one.png)
